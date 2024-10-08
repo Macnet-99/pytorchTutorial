24aabd0f35aa6b337d20de275dd309450ec44cda
Macnet-99 <157945116+Macnet-99@users.noreply.github.com>
2024-10-08 14:58:11 +0530

    Update 04_backpropagation.py

--- 04_backpropagation.py
+++ 04_backpropagation.py
@@ -3,7 +3,7 @@ import torch
 x = torch.tensor(1.0)
 y = torch.tensor(2.0)
 
-# This is the parameter we want to optimize -> requires_grad=True
+# This is the parameter we want to optimize -> requires_grad=True to set our gradient tracking
 w = torch.tensor(1.0, requires_grad=True)
 
 # forward pass to compute loss
