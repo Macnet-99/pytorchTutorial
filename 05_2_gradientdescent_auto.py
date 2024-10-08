0c6f459a69abb6ba6cacce50f92230c4482e9c86
Macnet-99 <157945116+Macnet-99@users.noreply.github.com>
2024-10-08 15:00:10 +0530

    Update 05_2_gradientdescent_auto.py

--- 05_2_gradientdescent_auto.py
+++ 05_2_gradientdescent_auto.py
@@ -5,7 +5,7 @@ import torch
 # Linear regression
 # f = w * x 
 
-# here : f = 2 * x
+# here : f = (2 * x)
 X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
 Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
 
